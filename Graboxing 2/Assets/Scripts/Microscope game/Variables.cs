d3fda5c42807a7ec1ba70a5107aeb11892132ec4
Kara2004 <karavdmeulen@gmail.com>
2025-05-22 14:15:41 +0200

    lenses texture

--- Graboxing 2/Assets/Scripts/Microscope game/Variables.cs
+++ Graboxing 2/Assets/Scripts/Microscope game/Variables.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.UI;
 
 public class Variables : MonoBehaviour
 {
     public static List<int> lenses = new List<int> { 1, 1, 1, 1, 1, 1 }; //adds the amount of lenses in a list with multiplier 1
     public static List<int> lens_catalogue = new List<int> { 25, 4, 2, 5, 20, 10 }; //adds all the individual multipliers if turned on
 
-    public float total_zoom;
+    public Image targetImage
     public TMP_Text lensText; //For the text to be added for "... X" multiplier 
 
     void Start()
@@ -27,4 +28,12 @@ public class Variables : MonoBehaviour
         lensText.text = "The microscope zooms in " + total_zoom + "X"; //Updates text with the new multiplier
     }
 
+    public void CheckZoom()
+    {
+        if (total_zoom <= 100)
+        {
+            
+        }
+
+    }
 }
