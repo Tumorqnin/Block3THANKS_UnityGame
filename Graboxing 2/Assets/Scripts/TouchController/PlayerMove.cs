cd6818fb81fb0c71403ecbea1d3ed8fa39237ff0
Tumorqnin <106229560+Tumorqnin@users.noreply.github.com>
2025-05-27 17:18:12 +0200

    Scene changing added

--- Graboxing 2/Assets/Scripts/TouchController/PlayerMove.cs
+++ Graboxing 2/Assets/Scripts/TouchController/PlayerMove.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
     public FixedJoystick joystick;
     public static float moveSpeed = 2f;
     public float gravity = -9.81f;
-    public float jumpHeight = 1.5f;
 
     private CharacterController controller;
     private Vector3 velocity;
@@ -15,17 +15,20 @@ public class PlayerMove : MonoBehaviour
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+
+    [SerializeField] private string sceneToLoad;
+    public static string m_SceneToLoad;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        m_SceneToLoad = sceneToLoad;
     }
 
     void Update()
     {
         // Check if the player is grounded
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance);
 
         if (isGrounded && velocity.y < 0)
         {
@@ -41,5 +44,11 @@ public class PlayerMove : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
-    // Optional: Add a jump method later if needed
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag.Equals("SceneChanger"))
+        {
+            SceneManager.LoadScene(m_SceneToLoad);
+        }
+    }
 }
